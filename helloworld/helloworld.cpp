eebab68089cb9cc974fc4233374c77cefd4d314d
jwei <jay.jiawei@outlook.com>
2016-07-20 11:35:47 -0400

    add helloworld sample project

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+  cout << "Hello World!" << endl;
+
+  string a = "aaa";
+  string b = a;
+  a = "ccc";
+
+  cout << a << endl << b << endl;
+
+  int ia = 1 << 0;  // ROLE_PARENT
+  int ib = 1 << 1;  // ROLE_SIBLING
+
+  string sIa = bitset<8>(ia).to_string();
+  string sIb = bitset<8>(ib).to_string();
+
+  cout << ia << " -- " << sIa << endl; // 01
+  cout << ib << " -- " << sIb << endl; // 10
+
+
+
+
+  return 0;
+}
