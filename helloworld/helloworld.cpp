16f2e3b0a6749b1cb0a8ab3208200c7fc688972b
jwei <jay.jiawei@outlook.com>
2016-07-20 18:10:30 -0400

    another modianother modi

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -5,14 +5,15 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  int ia = 1 << 0;  // ROLE_PARENT  01
-  int ib = 1 << 1;  // ROLE_SIBLING 10
+  int ia = 1 << 0; // ROLE_PARENT  01
+  int ib = 1 << 1; // ROLE_SIBLING 10
 
   string sIa = bitset<8>(ia).to_string();
   string sIb = bitset<8>(ib).to_string();
 
   cout << ia << " -- " << sIa << endl; // 01
   cout << ib << " -- " << sIb << endl; // 10
+
 
   return 0;
 }
